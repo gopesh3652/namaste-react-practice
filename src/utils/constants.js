538878c0fa920156ec9bb0239298ab7dc461d989
Gopesh Khandelwal <gopeshkhandelwal10@gmail.com>
2024-03-08 07:36:02 +0530

    cors in constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ export const LOGO_URL =
   "https://cdn.dribbble.com/userupload/9903003/file/original-8eae5aec60527b67b7678e42aa2d8645.jpg?resize=752x";
 
 export const RES_LIST =
-  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.7979769&lng=75.8192578&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.7979769&lng=75.8192578&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const RES_INFO =
-  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.7979769&lng=75.8192578&restaurantId=";
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=26.7979769&lng=75.8192578&restaurantId=";
